1eadc1c93fe3ac92ef97dab237146f9a3fb9d87f
Shohei Aoki <shoaok@gmail.com>
2016-01-06 12:56:41 +0900

    2次元ラプラス方程式による速度ポテンシャルの計算を実装

--- finite_difference_method/laplace2d_velocity_potential.m
+++ finite_difference_method/laplace2d_velocity_potential.m
@@ -13,10 +13,10 @@ drawDuct();
 global Phi;
 global deltaX; global deltaY;
 global iteration; global tolerance;
-iteration=50;
-tolerance=1e-3;
-deltaX = 0.01;
-deltaY = 0.01;
+iteration=5000;
+tolerance=1e-5;
+deltaX = 0.1;
+deltaY = 0.1;
 x = 0:deltaX:L;
 y = 0:deltaY:W;
 [X,Y] = meshgrid(x,y);
@@ -27,14 +27,16 @@ initVelocityPotential();
 
 computeGaussSeidelMethod();
 drawVelocityPotential();
+disp 'hello'
 
-    function drawDuct()
+    function drawDuct()        
         plot([0,Xobs-WobsX/2],[0,0],'k');
         plot([Xobs-WobsX/2,Xobs-WobsX/2],[0,WobsY],'k');
         plot([Xobs-WobsX/2,Xobs+WobsX/2],[WobsY,WobsY],'k');
         plot([Xobs+WobsX/2,Xobs+WobsX/2],[WobsY,0],'k');
         plot([Xobs+WobsX/2,L],[0,0],'k');
         plot([0,L],[W,W],'k')
+        title('VELOCITY POTENTIAL OF FLOW IN DUCT');
     end
 
     function initVelocityPotential()
@@ -70,66 +72,65 @@ drawVelocityPotential();
 
     function computeGaussSeidelMethod()
         cnt=0;
-        maxError=0;
+%         xmax=L/deltaX+1;
+%         ymax=W/deltaY+1;
+        
         while(cnt<iteration)
+            maxError=0.0;
             for iidx=1:L/deltaX+1
                 for jidx=1:W/deltaY+1
+                    
                     pType = checkPointAttribute(iidx,jidx);
                     switch(pType)
                         case 'OBSTACLE'
                             Phi(jidx,iidx)=0;
-                            continue;
                         case 'INTERNAL'
                             pn = (deltaY)^2*(Phi(jidx,iidx-1)+Phi(jidx,iidx+1))+...
                                 (deltaX)^2*(Phi(jidx-1,iidx)+Phi(jidx+1,iidx));
                             pd = 2*((deltaX)^2+(deltaY)^2);
                             pp = pn / pd;
+                            error = abs(pp - Phi(jidx,iidx));
+                            
+                            if(error > maxError)
+                                maxError = error;
+                                %                                 fprintf('update maxError %.10f @ i=%d j=%d\n',...
+                                %                                 maxError,iidx,jidx);
+                                Phi(jidx,iidx) = pp;
+                            end
                         case 'LEFT'
                             Phi(jidx,iidx) = 0;
-                            continue;
                         case 'RIGHT'
                             Phi(jidx,iidx) = U*L;
-                            continue;
                         case 'BOTTOM'
                             Phi(jidx,iidx) = Phi(jidx+1,iidx);
-                            continue;
                         case 'TOP'
                             Phi(jidx,iidx) = Phi(jidx-1,iidx);
-                            continue;
                         case 'OBS_LEFT'
                             Phi(jidx,iidx) = Phi(jidx,iidx-1);
-                            continue;
                         case 'OBS_TOP'
                             Phi(jidx,iidx) = Phi(jidx+1,iidx);
-                            continue;
                         case 'OBS_RIGHT'
                             Phi(jidx,iidx) = Phi(jidx,iidx+1);
-                            continue;
                         case 'CORNER_UPPER_LEFT'
                             Phi(jidx,iidx) = Phi(jidx-1,iidx-1);
-                            continue;
                         case 'CORNER_UPPER_RIGHT'
                             Phi(jidx,iidx) = Phi(jidx+1,iidx+1);
-                            continue;
-                    end
-                    
-                    error = abs(pp - Phi(jidx,iidx));
-                    
-                    if(error > maxError)
-                        maxError = error;
-                        %                         fprintf('update maxError %.5f @ i=%d j=%d\n',...
-                        %                             maxError,iidx,jidx);
-                        Phi(jidx,iidx) = pp;
                     end
                 end
             end
-               
-%             maxError
+            
+            %             maxError
             if(maxError<tolerance)
+                cnt
                 break;
             end
             cnt=cnt+1;
-            cnt
+            
+            cla;
+            str = sprintf('timeframe %d',cnt);
+            text(L-0.2,W+0.1,str);
+            drawVelocityPotential()
+            
         end
     end
 
@@ -137,40 +138,40 @@ drawVelocityPotential();
         if (iidx>((Xobs-WobsX/2)/deltaX)+1)&&(iidx<((Xobs+WobsX/2)/deltaX)+1)&&...
                 (jidx < (WobsY/deltaY)+1)
             attrib='OBSTACLE';
-            %             text((iidx-1)*deltaX,(jidx-1)*deltaY,'O');
-            %             drawnow;
+            %                         text((iidx-1)*deltaX,(jidx-1)*deltaY,'O');
+            %                         drawnow;
         elseif (jidx==1)
             attrib='BOTTOM';
             %             plot((iidx-1)*deltaX,(jidx-1)*deltaY,'bo');
-            %             text((iidx-1)*deltaX,(jidx-1)*deltaY,'B');
-            %             drawnow;
+            %                         text((iidx-1)*deltaX,(jidx-1)*deltaY,'B');
+            %                         drawnow;
         elseif (jidx==W/deltaY+1)
             attrib='TOP';
             %             plot((iidx-1)*deltaX,(jidx-1)*deltaY,'bo');
-            %             text((iidx-1)*deltaX,(jidx-1)*deltaY,'T');
-            %             drawnow;
+            %                         text((iidx-1)*deltaX,(jidx-1)*deltaY,'T');
+            %                         drawnow;
         elseif (iidx==1)
             attrib='LEFT';
             %             plot((iidx-1)*deltaX,(jidx-1)*deltaY,'bo');
-            %             text((iidx-1)*deltaX,(jidx-1)*deltaY,'L');
-            %             drawnow;
+            %                         text((iidx-1)*deltaX,(jidx-1)*deltaY,'L');
+            %                         drawnow;
         elseif (iidx==L/deltaX+1)
             attrib='RIGHT';
             %             plot((iidx-1)*deltaX,(jidx-1)*deltaY,'bo');
-            %             text((iidx-1)*deltaX,(jidx-1)*deltaY,'R');
-            %             drawnow;
+            %                         text((iidx-1)*deltaX,(jidx-1)*deltaY,'R');
+            %                         drawnow;
         elseif (iidx==(Xobs-WobsX/2)/deltaX+1)&&(jidx<(WobsY/deltaY+1))
             attrib='OBS_LEFT';
             %             plot((iidx-1)*deltaX,(jidx-1)*deltaY,'go');
-            %             text((iidx-1)*deltaX,(jidx-1)*deltaY,'O_L');
-            %             drawnow;
+            %                         text((iidx-1)*deltaX,(jidx-1)*deltaY,'O_L');
+            %                         drawnow;
         elseif (iidx>((Xobs-WobsX/2)/deltaX+1))&&...
                 (iidx<((Xobs+WobsX/2)/deltaX+1))&&...
                 (jidx==(WobsY/deltaY)+1)
             attrib='OBS_TOP';
             %             plot((iidx-1)*deltaX,(jidx-1)*deltaY,'go');
-            %             text((iidx-1)*deltaX,(jidx-1)*deltaY,'O_T');
-            %             drawnow;
+            %                         text((iidx-1)*deltaX,(jidx-1)*deltaY,'O_T');
+            %                         drawnow;
         elseif (iidx==(Xobs+WobsX/2)/deltaX+1)&&(jidx<(WobsY/deltaY+1))
             attrib='OBS_RIGHT';
             %             plot((iidx-1)*deltaX,(jidx-1)*deltaY,'go');
@@ -179,25 +180,26 @@ drawVelocityPotential();
         elseif (iidx==(Xobs-WobsX/2)/deltaX+1)&&(jidx==(WobsY/deltaY+1))
             attrib='CORNER_UPPER_LEFT';
             %             plot((iidx-1)*deltaX,(jidx-1)*deltaY,'ko');
-            %             text((iidx-1)*deltaX,(jidx-1)*deltaY,'C_L');
-            %             drawnow;
+            %                         text((iidx-1)*deltaX,(jidx-1)*deltaY,'C_L');
+            %                         drawnow;
         elseif (iidx==(Xobs+WobsX/2)/deltaX+1)&&(jidx==(WobsY/deltaY+1))
             attrib='CORNER_UPPER_RIGHT';
             %             plot((iidx-1)*deltaX,(jidx-1)*deltaY,'ko');
-            %             text((iidx-1)*deltaX,(jidx-1)*deltaY,'C_R');
-            %             drawnow;
+            %                         text((iidx-1)*deltaX,(jidx-1)*deltaY,'C_R');
+            %                         drawnow;
         else
             attrib='INTERNAL';
             %             plot(iidx*deltaX,jidx*deltaY,'ro');
-            %             text((iidx-1)*deltaX,(jidx-1)*deltaY,'I');
-            %             drawnow;
+            %                         text((iidx-1)*deltaX,(jidx-1)*deltaY,'I');
+            %                         drawnow;
         end
     end
 
 
     function drawVelocityPotential()
-            contour(Phi,Y,X,20,'LineColor','red');
-            drawnow;            
+        drawDuct();
+        contour(X,Y,Phi,10,'LineColor','red');
+        drawnow limitrate;
     end
 
 end
