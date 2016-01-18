089efc847baebedca91289a48ceb080f596924e7
Shohei Aoki <shoaok@gmail.com>
2016-01-18 20:34:25 +0900

    2次元拡散シミュレーションを追加

--- time_evolution/diffusion/diff2d_constD.m
+++ time_evolution/diffusion/diff2d_constD.m
@@ -2,21 +2,74 @@ function diff2d_constD
 clear all;
 close all;
 
-% shading interp
-% colormap(jet(8))
+% use surf() function for visualization
 
-xwidth=0.01;
-ywidth=0.01;
-xmax=1;
-ymax=1;
+xmax=2;
+ymax=2;
+deltaT = 0.1;
+D=0.05;
+% dx=D*(deltaT)/()
+% dy=
 
-x=-xmax:xwidth:xmax;
-y=-ymax:ywidth:ymax;
-[X,Y] = meshgrid(x,y);
-Z=-log(X.^2+Y.^2);
-% contour3(X,Y,Z,30);
-surf(X,Y,Z);
+xwidth=31;
+ywidth=31;
+x=linspace(-xmax,xmax,xwidth);
+diffx=diff(x);
+dx=diffx(1);
+y=linspace(-ymax,ymax,ywidth);
+diffy=diff(y);
+dy=diffy(1);
+[X,Y]=meshgrid(x,y);
+dix=D*deltaT/(dx)^2
+diy=D*deltaT/(dy)^2
 
+%initialize
+radius=1;
+Amp=xmax;
+f = Amp*(X.^2+Y.^2<=radius);
+% contour(X,Y,f)
+shading interp
+% shading flat;
+surf(X,Y,f);
+zlim([0 Amp*1.2]);
+% contourf(X,Y,f)
+% imagesc(f);
+axis equal;
+% az = 0;
+% el = 90;
+% view(az, el);
+colormap(hot);
+
+writerObj = VideoWriter('newfile.avi');
+open(writerObj);
+
+f_next=f;
+for t=1:30
+    for m=2:ywidth-1 %yscan
+        for n=2:xwidth-1 %xscan
+            f_next(m,n) = f(m,n) + dix*(f(m,n+1)-2*f(m,n)+f(m,n-1))+...
+                diy*(f(m+1,n)-2*f(m,n)+f(m-1,n));
+            f=f_next;
+        end
+
+        %     contourf(X,Y,f)
+        surf(X,Y,f);
+        zlim([0 Amp*1.2]);
+        colormap(hot);
+%         az = 0;
+%         el = 90;
+%         view(az, el);
+        drawnow;
+        zlim([0 Amp*1.2]);
+
+        frame = getframe(gcf);
+        writeVideo(writerObj, frame);
+
+    end
+    fprintf('t=%d \n',t);    
+end
+
+close(writerObj);
 
 
 end
